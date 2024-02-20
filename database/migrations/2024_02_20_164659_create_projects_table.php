9fdd289d100ca4391dbd092c2b7b05a770200001
CosimoA <cosimo.archip@gmail.com>
2024-02-20 18:12:20 +0100

    FEAT: add controller & migration

---
+++ database/migrations/2024_02_20_164659_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 64);
+            $table->string('used_technologies', 250);
+            $table->string('status', 64);
+            $table->text('description', 3000);
+            $table->string('web_site', 250);
+            $table->string('image_link', 250);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PortfolioController::class, 'index'])
+    ->name('projects.index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
